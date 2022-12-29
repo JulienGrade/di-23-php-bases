1023442b3e5f75b4fd2f250f2a0e5f7094cde45d
Julien Grade <juliengrade@macbook-pro-de-julien.home>
2022-12-29 18:13:53 +0100

    01-les-variables

---
+++ 02-les-variables/maPage.php
@@ -0,0 +1,12 @@
+<?php
+
+$nomDuJoueur1 = "Matthieu";
+$age = 30;
+echo "Nom Du joueur 1 : " . $nomDuJoueur1;
+echo "<br />";
+echo "age Du joueur 1 : " . $age;
+echo "<br />";
+++$age;
+echo "age Du joueur 1 : " . $age;
+
+?>
