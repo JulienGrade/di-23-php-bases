103620a93024bd50e095bda1057906bf611b2e90
Julien Grade <juliengrade@macbook-pro-de-julien.home>
2022-12-29 18:24:10 +0100

    03-les-types-de-variables

---
+++ 03-les-types-de-variables/maPage.php
@@ -0,0 +1,29 @@
+<?php
+// Type string
+$firstname = "Julien";
+
+echo 'La variable $firstname contient '.$firstname.' et elle est de type '.gettype($firstname);
+
+// Type integer
+$age = 39;
+
+echo $age.' est un '.gettype($age);
+
+// Type float
+$price = 12.99;
+
+echo $price.' est un '.gettype($age);
+
+// Type boolean
+$itIsTrue = true;
+$itIsFalse = false;
+
+echo $itIsTrue.', '.$itIsFalse.' sont des '.gettype($itIsTrue);
+
+// Type null
+$itIsNull = null;
+
+echo $itIsNull.' est de type '.gettype($itIsNull);
+
+
+?>
